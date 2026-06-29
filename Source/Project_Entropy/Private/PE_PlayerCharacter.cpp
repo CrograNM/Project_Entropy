687f3abedff0304e9d2db6bb046b13d283443ec5
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-06-29 16:01:18 +0900

    .

---
+++ Source/Project_Entropy/Private/PE_PlayerCharacter.cpp
@@ -0,0 +1,34 @@
+// Copyright CrograNM
+
+
+#include "PE_PlayerCharacter.h"
+
+// Sets default values
+APE_PlayerCharacter::APE_PlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APE_PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APE_PlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APE_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
