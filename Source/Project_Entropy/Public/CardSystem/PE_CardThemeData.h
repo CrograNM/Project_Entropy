ccb706fd4a34f5d84177715ee79211926a4def3a
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-07-27 01:00:47 +0900

    [CardSystem] - 이제 카드 액터는 CardThemeData에 저장된 등급x속성별 마법진을 가져옵니다.
    근데 속성(게임플레이태그)은 이제 만들어야함

--- Source/Project_Entropy/Public/CardSystem/PE_CardThemeData.h
+++ Source/Project_Entropy/Public/CardSystem/PE_CardThemeData.h
@@ -9,7 +9,21 @@
 #include "PE_CardThemeData.generated.h"
 
 class UTexture2D;
+class UNiagaraSystem;
 class UMaterialInstance;
+
+/**
+ * 특정 등급(Rarity) 내에 존재하는 속성별 마법진 이펙트를 담는 구조체
+ */
+USTRUCT(BlueprintType)
+struct FPE_ElementVFXMap
+{
+	GENERATED_BODY()
+
+	// 속성(ElementTag) -> 나이아가라 이펙트 매핑
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Theme|VFX")
+	TMap<FGameplayTag, TObjectPtr<UNiagaraSystem>> ElementVFXs;
+};
 
 /**
  * 카드의 등급(Rarity) 및 속성(Element)에 따른 UI 색상과 프레임 에셋을
@@ -35,4 +49,11 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Theme|Element")
 	TMap<FGameplayTag, FLinearColor> ElementColorMap;
+
+	/**
+	 * [등급별 -> 속성별] 마법진 이펙트 매핑
+	 * 예: 전설(Key) -> [불 속성 마법진, 물 속성 마법진 ...](Value)
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Theme|VFX")
+	TMap<EPECardRarity, FPE_ElementVFXMap> CastingMagicCircleMap;
 };
